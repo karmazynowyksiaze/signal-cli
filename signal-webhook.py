2262ede6ccf3023f74d3cb46e2155da7eaacac53
karmazynowyksiaze <ppawlowski186@gmail.com>
2025-07-21 11:46:57 +0200

    add api_key authorization

--- signal-webhook.py
+++ signal-webhook.py
@@ -20,12 +20,27 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 SIGNAL_NUMBER = os.environ.get('SIGNAL_NUMBER')
 SIGNAL_GROUP_ID = os.environ.get('SIGNAL_GROUP_ID')
+API_KEY= os.environ.get('WEBHOOK_API_KEY')
 
 app = Flask(__name__)
 
+#Authorization API
+def is_authorized(req):
+    auth_header = req.headers.get("Authorization", "")
+    if auth_header.startsith("Bearer "):
+        token = auth_header.replace("Bearer ", "").strip()
+        return token in API_KEY
+    return False
+
 @app.route('/send', methods=['POST'])
 def send_signal_message():
+    if not is_authorized():
+        logger.warning("Unathorized access attempt")
+        return jsonify({'error': 'Unathorized'}), 401
+    
     data = request.json
+    logger.info(f"Received data: {data}")
+
     if not data or 'message' not in data:
         logger.warning('Missing message')
         return jsonify({'error': 'Missing message'}), 400
